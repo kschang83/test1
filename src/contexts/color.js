ee34387c7f142729824a306f5d0bf0e3220c0cbf
kschang <kschang83@gmail.com>
2019-10-14 15:08:39 +0900

    2nd commint MAC

--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react';
 
 const ColorContext = createContext({
-    state: { color: 'black', subcolor: 'red' },
+    state: { color: 'gray', subcolor: 'blue' },
     actions: {
         setColor: () => { },
         setSubcolor: () => { }
