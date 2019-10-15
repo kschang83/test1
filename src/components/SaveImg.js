9272de9ae077867edc7121519d346699d30be525
cks83 <kschang83@gmail.com>
2019-10-15 13:31:16 +0900

    111

--- src/components/SaveImg.js
+++ src/components/SaveImg.js
@@ -6,8 +6,8 @@ const SaveImg = () => {
     const handleClick = (e) => {
         html2canvas(document.querySelector("#canvas_div2")).then(function (canvas) {
             document.body.appendChild(canvas);
-            let canvasImg = canvas.toDataURL();
-            console.log(canvasImg);
+            let canvasImgs = canvas.toDataURL();
+            console.log(canvasImgs);
         });
     }
 
