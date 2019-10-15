5c35077681326910e9147fa667c63a61d2e8602b
kschang <kschang83@gmail.com>
2019-10-15 13:22:00 +0900

    ssss

--- src/components/SaveImg.js
+++ src/components/SaveImg.js
@@ -4,10 +4,9 @@ import html2canvas from 'html2canvas';
 const SaveImg = () => {
 
     const handleClick = (e) => {
-        html2canvas(document.querySelector("#canvas_div2")).then(function (canvas) {
+        html2canvas(document.querySelector("#canvas_div")).then(function (canvas) {
             document.body.appendChild(canvas);
-            let myImages = canvas.toDataURL();
-            console.log(myImages);
+            let aaa = canvas.toDataURL();
         });
     }
 
