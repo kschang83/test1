427d9127893cc868dcf2c5f26d0c89b7616de22e
kschang <kschang83@gmail.com>
2019-10-15 11:17:11 +0900

    dadsa

--- src/components/SaveImg.js
+++ src/components/SaveImg.js
@@ -6,8 +6,8 @@ const SaveImg = () => {
     const handleClick = (e) => {
         html2canvas(document.querySelector("#canvas_div2")).then(function (canvas) {
             document.body.appendChild(canvas);
-            let myImageURL0099 = canvas.toDataURL();
-            console.log(myImageURL0099);
+            let myImageURL0099dsa = canvas.toDataURL();
+            console.log(myImageURL0099dsa);
         });
     }
 
